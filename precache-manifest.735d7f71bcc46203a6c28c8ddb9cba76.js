ea6f0e4392d336119732eb9151432919f75e34f5
pentwo <pentwo@gmail.com>
2021-02-06 15:13:23 +0800

    Updates

---
+++ precache-manifest.735d7f71bcc46203a6c28c8ddb9cba76.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d27fbf9f5fa098bd350137bd45b0c89e",
+    "url": "/poadcast-search-and-play/index.html"
+  },
+  {
+    "revision": "dc39e4053d9881f7ee9f",
+    "url": "/poadcast-search-and-play/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "bbd76f85019c80460814",
+    "url": "/poadcast-search-and-play/static/css/main.2774fc0f.chunk.css"
+  },
+  {
+    "revision": "dc39e4053d9881f7ee9f",
+    "url": "/poadcast-search-and-play/static/js/2.bd16200f.chunk.js"
+  },
+  {
+    "revision": "dfc349601c4d7f44243920bdc9b8c8a6",
+    "url": "/poadcast-search-and-play/static/js/2.bd16200f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbd76f85019c80460814",
+    "url": "/poadcast-search-and-play/static/js/main.56f4ca84.chunk.js"
+  },
+  {
+    "revision": "3b388dfcbc13f0c9d34a",
+    "url": "/poadcast-search-and-play/static/js/runtime-main.f0ae2885.js"
+  },
+  {
+    "revision": "798481ceae4a2772d3462b8f9210f1a8",
+    "url": "/poadcast-search-and-play/static/media/dual-ring-1s-200px.798481ce.svg"
+  },
+  {
+    "revision": "8294751b0bafa0cd37784dcd43c8bcae",
+    "url": "/poadcast-search-and-play/static/media/ic-delete-48px.8294751b.svg"
+  },
+  {
+    "revision": "618f02f0ca6408c4bc37f924daf8d05b",
+    "url": "/poadcast-search-and-play/static/media/ic-forward-30-48px.618f02f0.svg"
+  },
+  {
+    "revision": "43974475eea303bdff1b111c58c77c5a",
+    "url": "/poadcast-search-and-play/static/media/ic-pause-48px.43974475.svg"
+  },
+  {
+    "revision": "0d38baf74f31d4c6b1365f95d7eb2d10",
+    "url": "/poadcast-search-and-play/static/media/ic-play-arrow-48px.0d38baf7.svg"
+  },
+  {
+    "revision": "a542b1eb54f49d9836cd10a640062563",
+    "url": "/poadcast-search-and-play/static/media/ic-playlist-add-48px.a542b1eb.svg"
+  },
+  {
+    "revision": "d922df0477f347bcee2af28fbe883c5d",
+    "url": "/poadcast-search-and-play/static/media/ic-replay-30-48px.d922df04.svg"
+  },
+  {
+    "revision": "673d423303e761c26b1cc947d829d782",
+    "url": "/poadcast-search-and-play/static/media/ic-skip-next-48px.673d4233.svg"
+  },
+  {
+    "revision": "33ac048861791bfa8de2abf86be6a10f",
+    "url": "/poadcast-search-and-play/static/media/sound-nobg.33ac0488.gif"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/poadcast-search-and-play/precache-manifest.735d7f71bcc46203a6c28c8ddb9cba76.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/poadcast-search-and-play/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
